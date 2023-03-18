8f8b78fce9ef63b468c897a284e186a50d0f578c
amrokh1996 <amrrrkh1996@gmail.com>
2023-03-18 17:58:11 +0300

    upload task

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Caffe-Z
+> ### `Introduction:`
+In this task, we have created a web page for the coffee shop, in addition to a place to order drinks inside the site using JavaScript.
+---
+
+> ### `Used Tools:`
++ VS code.
++ Github>
+---
+> ### `Used Languages:`
++ HTML
++ CSS
++ Bootstrap
++ JavaScript
+---
+
+> ### `Links:`
++ [live link]()
+
---
+++ app.js
@@ -0,0 +1,24 @@
+// let name =prompt("Enter your name");
+// const Gender=prompt("Enter your gender");
+
+// if(Gender =="male"){
+//     name="Mr. "+name;
+//     alert("Welcome "+name)
+// }else if(Gender =="female"){
+//     name="Mr. "+name;
+//     alert("Welcome "+name)
+// }else{
+//     alert("Welcome "+name) 
+// }
+
+
+// const drinkType =prompt("Would you like a hot or cold drink?");
+// const drink =prompt("What do want to drink?");
+// alert("The drink will be prepared soon \n please wait...")
+
+// console.log("Name: "+name);
+// console.log("Your drink: " + drinkType+" "+drink)
+
+
+
+
