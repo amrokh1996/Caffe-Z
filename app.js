b3b6006f5f7b3a170fd4a79af83aacad28afab1b
amrokh1996 <amrrrkh1996@gmail.com>
2023-03-18 17:59:41 +0300

    modify js code

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-// let name =prompt("Enter your name");
-// const Gender=prompt("Enter your gender");
+let name =prompt("Enter your name");
+const Gender=prompt("Enter your gender");
 
-// if(Gender =="male"){
-//     name="Mr. "+name;
-//     alert("Welcome "+name)
-// }else if(Gender =="female"){
-//     name="Mr. "+name;
-//     alert("Welcome "+name)
-// }else{
-//     alert("Welcome "+name) 
-// }
+if(Gender =="male"){
+    name="Mr. "+name;
+    alert("Welcome "+name)
+}else if(Gender =="female"){
+    name="Mr. "+name;
+    alert("Welcome "+name)
+}else{
+    alert("Welcome "+name) 
+}
 
 
-// const drinkType =prompt("Would you like a hot or cold drink?");
-// const drink =prompt("What do want to drink?");
-// alert("The drink will be prepared soon \n please wait...")
+const drinkType =prompt("Would you like a hot or cold drink?");
+const drink =prompt("What do want to drink?");
+alert("The drink will be prepared soon \n please wait...")
 
-// console.log("Name: "+name);
-// console.log("Your drink: " + drinkType+" "+drink)
+console.log("Name: "+name);
+console.log("Your drink: " + drinkType+" "+drink)
 
 
 
