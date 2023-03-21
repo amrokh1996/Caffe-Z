db30461efd88bb5bbb33b10549b36756440ede92
amrokh1996 <amrrrkh1996@gmail.com>
2023-03-21 21:44:01 +0300

    upload dom task section

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var name =prompt("Enter your name");
 var Gender =prompt("Enter your gender");
 
 genderFun(Gender);
+var age =prompt("Enter your age");
 
 const drinkType =prompt("Would you like a hot or cold drink?");
 const drink =prompt("What do want to drink?");
@@ -15,6 +16,34 @@ arr.push(name,Gender,drinkType,drink)
 
 for(let x=0;x<arr.length;x++){
     console.log(arr[x])
+}
+
+let order=document.getElementById("order");
+let parg = document.createElement("p");
+
+let ull= document.createElement("ul");
+
+let li1 = document.createElement("li");
+let li2 = document.createElement("li");
+let li3 = document.createElement("li");
+
+parg.textContent=name;
+li1.textContent = "Gender: "+Gender
+li2.textContent = "Age: "   + age
+li3.textContent = "drink: " + drinkType +" "+drink
+
+ull.appendChild(li1);
+ull.appendChild(li2);
+ull.appendChild(li3);
+order.appendChild(parg);
+order.appendChild(ull)
+
+document.getElementById("order").style.display="block";
+
+
+function closefun(){
+    console.log("sdfdsfsd")
+    document.getElementById("order").style.display="none";
 }
 
 function genderFun(){
